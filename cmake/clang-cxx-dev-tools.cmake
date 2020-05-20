a3af94d0ba4e1550b6b05f2e411e66f057c2b9c7
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2020-05-19 20:05:00 -0700

    [fix]: Clang-format Exclude build directory from formatting
    Details:
    Adding logic to exclude build directory and enabled verbose
    
    Resolves:
     * Formatting latency
    
    Tests:
     * Locally tested by running `make format`
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -1,9 +1,19 @@
 # Additional targets to perform clang-format/clang-tidy
+#Eclude files in build directory
+set (EXCLUDE_DIR "/build/")
 # Get all project files
 file(GLOB_RECURSE
      ALL_CXX_SOURCE_FILES
      *.[chi]pp *.[chi]xx *.h *.c *.cc *.hh *.ii *.[CHI]
      )
+
+# Remove files in build directory
+foreach (TMP_PATH ${ALL_CXX_SOURCE_FILES})
+    string (FIND ${TMP_PATH} ${EXCLUDE_DIR} EXCLUDE_DIR_FOUND)
+    if (NOT ${EXCLUDE_DIR_FOUND} EQUAL -1)
+        list (REMOVE_ITEM ALL_CXX_SOURCE_FILES ${TMP_PATH})
+    endif ()
+endforeach(TMP_PATH)
 
 # Adding clang-format target if executable is found
 find_program(CLANG_FORMAT "clang-format")
@@ -13,6 +23,7 @@ if(CLANG_FORMAT)
     COMMAND clang-format
     -i
     -style=file
+    --verbose 
     ${ALL_CXX_SOURCE_FILES}
     )
 endif()
